37f8c1f3e6e35c66ac64a787c0240d547b2dbdd0
Wojciech Zięba <wojciech.zieba@protonmail.com>
2021-10-21 16:42:30 +0200

    Initial commit

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,14 @@
+object Versions {
+    const val JUNIT = "4.13.2"
+}
+
+object BuildPluginsVersion {
+    const val DETEKT = "1.16.0"
+    const val KOTLIN = "1.4.32"
+    const val KTLINT = "10.0.0"
+    const val VERSIONS_PLUGIN = "0.38.0"
+}
+
+object TestingLib {
+    const val JUNIT = "junit:junit:${Versions.JUNIT}"
+}
