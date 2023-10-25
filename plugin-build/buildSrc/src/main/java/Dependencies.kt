a08860efb2a498a337c46995ace115051f4a8071
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-10-25 16:01:31 +0200

    build: bump Kotlin to 1.9.10

--- plugin-build/buildSrc/src/main/java/Dependencies.kt
+++ plugin-build/buildSrc/src/main/java/Dependencies.kt
@@ -4,7 +4,7 @@ object Versions {
 
 object BuildPluginsVersion {
     const val DETEKT = "1.16.0"
-    const val KOTLIN = "1.5.31"
+    const val KOTLIN = "1.9.10"
     const val KTLINT = "10.0.0"
     const val PLUGIN_PUBLISH = "0.14.0"
     const val VERSIONS_PLUGIN = "0.38.0"
