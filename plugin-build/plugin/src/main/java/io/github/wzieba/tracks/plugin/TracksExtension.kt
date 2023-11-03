bc1a8bc9d6b173e7fbc22767d2a9371fb23eeed9
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-11-03 09:53:04 +0100

    Add new supported projects^

--- plugin-build/plugin/src/main/java/io/github/wzieba/tracks/plugin/TracksExtension.kt
+++ plugin-build/plugin/src/main/java/io/github/wzieba/tracks/plugin/TracksExtension.kt
@@ -14,6 +14,6 @@ abstract class TracksExtension @Inject constructor(project: Project) {
     val enabled: Property<Boolean> = objects.property(Boolean::class.java)
 
     enum class AutomatticProject {
-        WooCommerce, WordPress, DayOne
+        WooCommerce, WordPress, DayOne, PocketCasts, Tumblr, FluxC
     }
 }
