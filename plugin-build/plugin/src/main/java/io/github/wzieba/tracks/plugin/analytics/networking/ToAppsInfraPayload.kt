741550b338d87e41a6090bf767cf00e6383b78a6
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-11-03 11:05:35 +0100

    Update schema of sent payload to a new format

--- plugin-build/plugin/src/main/java/io/github/wzieba/tracks/plugin/analytics/networking/ToAppsInfraPayload.kt
+++ plugin-build/plugin/src/main/java/io/github/wzieba/tracks/plugin/analytics/networking/ToAppsInfraPayload.kt
@@ -3,101 +3,43 @@ package io.github.wzieba.tracks.plugin.analytics.networking
 import io.github.wzieba.tracks.plugin.BuildData
 
 @Suppress("LongMethod")
-fun BuildData.toAppsInfraPayload(user: String) = GroupedAppsMetrics(
-    meta = listOf(
-        AppsMetric(
-            name = "User",
-            value = user
-        ),
-        AppsMetric(
-            name = "Project",
-            value = this.forProject.name
-        )
-    ),
-    metrics = listOf(
-        AppsMetric(
-            name = "Requested tasks",
-            value = this.tasks.joinToString(separator = ",")
-        ),
-        AppsMetric(
-            name = "Performed Gradle action",
-            value = this.action
-        ),
-        AppsMetric(
-            name = "Build time (ms)",
-            value = this.buildTime.toString()
-        ),
-        AppsMetric(
-            name = "Build status",
-            value = if (this.failed) "Failure" else "Success"
-        ),
-        AppsMetric(
-            name = "Failure message",
-            value = this.failure.toString()
-        ),
-        AppsMetric(
-            name = "Number of running daemons",
-            value = this.daemonsRunning.toString()
-        ),
-        AppsMetric(
-            name = "Daemon's build count",
-            value = this.thisDaemonBuilds.toString()
-        ),
-        AppsMetric(
-            name = "Gradle version",
-            value = this.gradleVersion
-        ),
-        AppsMetric(
-            name = "Operating system",
-            value = this.operatingSystem
-        ),
-        AppsMetric(
-            name = "Environment",
-            value = this.environment.toString()
-        ),
-        AppsMetric(
-            name = "Total number of tasks",
-            value = this.taskStatistics.total.toString()
-        ),
-        AppsMetric(
-            name = "Up to date tasks",
-            value = this.taskStatistics.upToDate.toString()
-        ),
-        AppsMetric(
-            name = "Tasks from cache",
-            value = this.taskStatistics.fromCache.toString()
-        ),
-        AppsMetric(
-            name = "Executed tasks",
-            value = this.taskStatistics.executed.toString()
-        ),
-        AppsMetric(
-            name = "Is configure on demand",
-            value = this.isConfigureOnDemand.toString()
-        ),
-        AppsMetric(
-            name = "Is configuration cache",
-            value = this.isConfigurationCache.toString()
-        ),
-        AppsMetric(
-            name = "Is build cache",
-            value = this.isBuildCache.toString()
-        ),
-        AppsMetric(
-            name = "Max workers",
-            value = this.maxWorkers.toString()
-        ),
-        AppsMetric(
-            name = "Build data collection overhead",
-            value = this.buildDataCollectionOverhead.toString()
-        ),
-        AppsMetric(
-            name = "Included builds",
-            value = this.includedBuildsNames.joinToString(separator = ",").ifEmpty { "none" }
-        ),
-        AppsMetric(
-            name = "Architecture",
-            value = this.architecture
-        ),
+fun BuildData.toAppsInfraPayload(user: String): GroupedAppsMetrics {
+    val projectKey = this.forProject.name.lowercase()
+
+    val meta = mapOf(
+        "user" to user, "project" to projectKey
     )
-)
+
+    val metrics = mapOf(
+        "requested-tasks" to tasks.joinToString(separator = ","),
+        "performed-gradle-action" to action,
+        "build-time-ms" to buildTime.toString(),
+        "build-status" to if (failed) "Failure" else "Success",
+        "failure-message" to failure.toString(),
+        "number-of-running-daemons" to daemonsRunning.toString(),
+        "daemons-build-count" to thisDaemonBuilds.toString(),
+        "gradle-version" to gradleVersion,
+        "operating-system" to operatingSystem,
+        "environment" to environment.name,
+        "total-tasks-number" to taskStatistics.total.toString(),
+        "up-to-date-tasks" to taskStatistics.upToDate.toString(),
+        "cached-tasks" to taskStatistics.fromCache.toString(),
+        "executed-tasks" to taskStatistics.executed.toString(),
+        "configure-on-demand" to isConfigureOnDemand.toString(),
+        "configuration-cache" to isConfigurationCache.toString(),
+        "build-cache" to isBuildCache.toString(),
+        "max-workers" to maxWorkers.toString(),
+        "build-data-collection-overhead-ms" to buildDataCollectionOverhead.toString(),
+        "included-builds" to includedBuildsNames.joinToString(separator = ",").ifEmpty { "none" },
+        "architecture" to architecture
+    )
+
+    return GroupedAppsMetrics(
+        meta = meta.map { (key, value) -> AppsMetric(name = "$projectKey-$key", value = value) },
+        metrics = metrics.map { (key, value) ->
+            AppsMetric(
+                name = "$projectKey-$key", value = value
+            )
+        },
+    )
+}
