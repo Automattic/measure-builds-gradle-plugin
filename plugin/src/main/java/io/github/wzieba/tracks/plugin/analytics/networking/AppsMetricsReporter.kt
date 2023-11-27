121dcef0910463d85f3dee3f82ace6b36aa09381
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-11-27 16:37:03 +0100

    feat: do not crash build in case of failed metrics upload

--- plugin/src/main/java/io/github/wzieba/tracks/plugin/analytics/networking/AppsMetricsReporter.kt
+++ plugin/src/main/java/io/github/wzieba/tracks/plugin/analytics/networking/AppsMetricsReporter.kt
@@ -4,6 +4,7 @@ import io.github.wzieba.tracks.plugin.Report
 import io.github.wzieba.tracks.plugin.analytics.AnalyticsReporter
 import io.github.wzieba.tracks.plugin.analytics.Emojis.FAILURE_ICON
 import io.github.wzieba.tracks.plugin.analytics.Emojis.SUCCESS_ICON
+import io.github.wzieba.tracks.plugin.analytics.Emojis.WAITING_ICON
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.features.HttpTimeout
@@ -34,60 +35,66 @@ class AppsMetricsReporter(
         authToken: String,
         gradleScanId: String?
     ) {
-        logger.debug("Reporting $report")
+        try {
+            logger.debug("Reporting $report")
 
-        val client = HttpClient(CIO) {
-            install(Logging) {
-                this.logger = object : io.ktor.client.features.logging.Logger {
-                    override fun log(message: String) {
-                        this@AppsMetricsReporter.logger.debug(message)
+            val client = HttpClient(CIO) {
+                install(Logging) {
+                    this.logger = object : io.ktor.client.features.logging.Logger {
+                        override fun log(message: String) {
+                            this@AppsMetricsReporter.logger.debug(message)
+                        }
                     }
+                    level = io.ktor.client.features.logging.LogLevel.ALL
                 }
-                level = io.ktor.client.features.logging.LogLevel.ALL
+                install(JsonFeature) {
+                    serializer = KotlinxSerializer()
+                }
+                install(HttpTimeout) {
+                    requestTimeoutMillis = 5.seconds.inWholeMilliseconds
+                }
             }
-            install(JsonFeature) {
-                serializer = KotlinxSerializer()
-            }
-            install(HttpTimeout) {
-                requestTimeoutMillis = 5.seconds.inWholeMilliseconds
-            }
-        }
 
-        client.post<HttpStatement>("https://seal-app-e8plp.ondigitalocean.app/api/grouped-metrics") {
-            headers {
-                append(HttpHeaders.UserAgent, "Gradle")
-                append(Authorization, "Bearer $authToken")
-            }
-            contentType(ContentType.Application.Json)
-            body = report.toAppsInfraPayload(gradleScanId)
-        }.execute { response: HttpResponse ->
-            logger.debug(response.toString())
+            client.post<HttpStatement>("https://seal-app-e8plp.ondigitalocean.app/api/grouped-metrics") {
+                headers {
+                    append(HttpHeaders.UserAgent, "Gradle")
+                    append(Authorization, "Bearer $authToken")
+                }
+                contentType(ContentType.Application.Json)
+                body = report.toAppsInfraPayload(gradleScanId)
+            }.execute { response: HttpResponse ->
+                logger.debug(response.toString())
 
-            when (response.status) {
-                HttpStatusCode.Created -> {
-                    val buildTime = report.executionData.buildTime
-                    val timeFormatted = String.format(
-                        Locale.US,
-                        "%dm %ds",
-                        MILLISECONDS.toMinutes(buildTime),
-                        MILLISECONDS.toSeconds(buildTime) - MINUTES.toSeconds(
-                            MILLISECONDS.toMinutes(
-                                buildTime
+                when (response.status) {
+                    HttpStatusCode.Created -> {
+                        val buildTime = report.executionData.buildTime
+                        val timeFormatted = String.format(
+                            Locale.US,
+                            "%dm %ds",
+                            MILLISECONDS.toMinutes(buildTime),
+                            MILLISECONDS.toSeconds(buildTime) - MINUTES.toSeconds(
+                                MILLISECONDS.toMinutes(
+                                    buildTime
+                                )
                             )
                         )
-                    )
-                    logger.lifecycle(
-                        "\n$SUCCESS_ICON Build time report of $timeFormatted has been received by App Metrics."
-                    )
-                }
+                        logger.lifecycle(
+                            "\n$SUCCESS_ICON Build time report of $timeFormatted has been received by App Metrics."
+                        )
+                    }
 
-                else -> {
-                    logger.warn(
-                        "\n$FAILURE_ICON Build time has not been uploaded. Add `debug` property to see more logs."
-                    )
+                    else -> {
+                        logger.warn(
+                            "\n$FAILURE_ICON Build time has not been uploaded. Add `debug` property to see more logs."
+                        )
+                    }
                 }
             }
+            client.close()
+        } catch (exception: Exception){
+            logger.warn(
+                "\n$FAILURE_ICON Build time has not been uploaded. Add `debug` property to see more logs."
+            )
         }
-        client.close()
     }
 }
