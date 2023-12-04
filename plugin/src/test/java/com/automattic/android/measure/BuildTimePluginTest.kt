38a0d3552e8d2d8cc2c8ec6adac938243528426e
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-12-04 11:44:31 +0100

    tests: add unit test to verify enable=null when not attaching scan id

--- plugin/src/test/java/com/automattic/android/measure/BuildTimePluginTest.kt
+++ plugin/src/test/java/com/automattic/android/measure/BuildTimePluginTest.kt
@@ -13,8 +13,7 @@ class BuildTimePluginTest {
     fun `given a project that attaches gradle scan id, when executing a task with configuration from cache, then send the report with attached gradle scan id`() {
         // given
         val runner = functionalTestRunner(
-            enable = true,
-            attachGradleScanId = true
+            enable = true, attachGradleScanId = true
         )
 
         // when
@@ -22,8 +21,7 @@ class BuildTimePluginTest {
             runner.withArguments("--configuration-cache", "help").build()
 
         // then
-        assertThat(prepareConfigurationCache.output)
-            .contains("Calculating task graph as no configuration cache is available for tasks")
+        assertThat(prepareConfigurationCache.output).contains("Calculating task graph as no configuration cache is available for tasks")
             .contains("Configuration cache entry stored")
 
         // when
@@ -31,8 +29,7 @@ class BuildTimePluginTest {
             runner.withArguments("--configuration-cache", "help", "--debug").build()
 
         // then
-        assertThat(buildUsingConfigurationCache.output)
-            .contains("Reusing configuration cache")
+        assertThat(buildUsingConfigurationCache.output).contains("Reusing configuration cache")
             .contains("Reporting build data to Apps Metrics...")
             .contains("{\"name\":\"woocommerce-gradle-scan-id\",\"value\":")
             .doesNotContain("{\"name\":\"woocommerce-gradle-scan-id\",\"value\":\"null\"}")
@@ -44,15 +41,28 @@ class BuildTimePluginTest {
         val runner = functionalTestRunner(
             enable = false,
             attachGradleScanId = false,
-
         )
 
         // when
         val run = runner.withArguments("help").build()
 
         // then
-        assertThat(run.output)
-            .doesNotContain("Reporting build data to Apps Metrics...")
+        assertThat(run.output).doesNotContain("Reporting build data to Apps Metrics...")
+    }
+
+    @Test
+    fun `given a project that did not enable build measurements and does not attach Gradle Scan Id, when executing a task, then build metrics are not sent`() {
+        // given
+        val runner = functionalTestRunner(
+            enable = null,
+            attachGradleScanId = false,
+        )
+
+        // when
+        val run = runner.withArguments("help").build()
+
+        // then
+        assertThat(run.output).doesNotContain("Reporting build data to Apps Metrics...")
     }
 
     @BeforeEach
@@ -62,9 +72,7 @@ class BuildTimePluginTest {
     }
 
     private fun functionalTestRunner(
-        enable: Boolean,
-        attachGradleScanId: Boolean,
-        vararg arguments: String
+        enable: Boolean?, attachGradleScanId: Boolean, vararg arguments: String
     ): GradleRunner {
         val projectDir = File("build/functionalTest")
         projectDir.mkdirs()
@@ -88,7 +96,10 @@ class BuildTimePluginTest {
                 id("com.automattic.android.measure-builds")
             }
             measureBuilds {
-                enable.set($enable)
+                ${
+                if (enable != null) "enable.set($enable)"
+                else ""
+            }
                 attachGradleScanId.set($attachGradleScanId)
                 automatticProject.set(com.automattic.android.measure.MeasureBuildsExtension.AutomatticProject.WooCommerce)
             }
