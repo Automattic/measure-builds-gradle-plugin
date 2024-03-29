bc38b033603d21f5ea789f2a9d4cab7ed4f78e71
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-03-29 09:42:45 +0100

    docs: add Javadoc comments for plugin extension

--- measure-builds/src/main/java/com/automattic/android/measure/MeasureBuildsExtension.kt
+++ measure-builds/src/main/java/com/automattic/android/measure/MeasureBuildsExtension.kt
@@ -9,14 +9,23 @@ abstract class MeasureBuildsExtension(project: Project) {
 
     private val objects = project.objects
 
+    /**
+     * Enable or disable the plugin.
+     */
     val enable: Property<Boolean> = objects.property(Boolean::class.java)
 
+    /**
+     * Enable or disable obfuscation of the username. If `true`, the username will be obfuscated by SHA-1.
+     */
     val obfuscateUsername: Property<Boolean> = objects.property(Boolean::class.java)
 
     @Suppress("UNCHECKED_CAST")
     internal val buildMetricsPreparedAction: Property<Action<MetricsReport>> =
         objects.property(Action::class.java) as Property<Action<MetricsReport>>
 
+    /**
+     * Action to be executed when the build metrics are ready.
+     */
     fun buildMetricsPrepared(action: Action<MetricsReport>) {
         buildMetricsPreparedAction.set(action)
     }
@@ -27,7 +36,7 @@ abstract class MeasureBuildsExtension(project: Project) {
      * Gradle Build Scan id to metrics.
      *
      * If `false`, then metrics will be sent at build finish by
-     * @see io.github.wzieba.tracks.plugin.analytics.BuildFinishedFlowAction
+     * @see [com.automattic.android.measure.lifecycle.BuildFinishedFlowAction].
      */
     val attachGradleScanId: Property<Boolean> = objects.property(Boolean::class.java)
 }
