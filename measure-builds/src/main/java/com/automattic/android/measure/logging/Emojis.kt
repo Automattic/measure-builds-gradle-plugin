85e36798d13f8437069eb863cdb288a04dd02960
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-03-25 17:16:35 +0100

    Improve remote build cache logging, print at most 3 best savings

--- measure-builds/src/main/java/com/automattic/android/measure/logging/Emojis.kt
+++ measure-builds/src/main/java/com/automattic/android/measure/logging/Emojis.kt
@@ -5,4 +5,5 @@ object Emojis {
     const val FAILURE_ICON = "\u274C"
     const val WAITING_ICON = "\u23F3"
     const val TURTLE_ICON = "\uD83D\uDC22"
+    const val ROCKET_ICON = "\uD83D\uDE80"
 }
--- measure-builds/src/main/java/com/automattic/android/measure/reporters/RemoteBuildCacheMetricsReporter.kt
+++ measure-builds/src/main/java/com/automattic/android/measure/reporters/RemoteBuildCacheMetricsReporter.kt
@@ -1,11 +1,15 @@
 package com.automattic.android.measure.reporters
 
+import com.automattic.android.measure.logging.Emojis
 import org.gradle.api.logging.Logging
+import java.util.Locale
+import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 
 object RemoteBuildCacheMetricsReporter {
 
     private val logger = Logging.getLogger(RemoteBuildCacheMetricsReporter::class.java)
+    private val LOG_SAVINGS_THRESHOLD = 500.milliseconds
 
     @JvmStatic
     fun report(metricsReporter: MetricsReport) {
@@ -19,9 +23,18 @@ object RemoteBuildCacheMetricsReporter {
             } else {
                 null
             }
+        }.sortedByDescending { it.second }
+
+        val totalSavings = avoidanceMap.sumOf { it.second.inWholeMilliseconds }.milliseconds
+
+        if (totalSavings > LOG_SAVINGS_THRESHOLD) {
+            logger.lifecycle("\n${Emojis.ROCKET_ICON} Sum of estimated remote cache savings: $totalSavings (not actual saved build duration). Top savings:")
+            logger.lifecycle(String.format(Locale.US, "%-15s %s", "Saved", "Task"))
+            avoidanceMap.take(3).forEach { (task, avoidance) ->
+                logger.lifecycle(String.format("%-15s %s", avoidance, task))
+            }
+        } else if (totalSavings < Duration.ZERO) {
+            logger.lifecycle("\n${Emojis.FAILURE_ICON} It's estimated that remote cache added ${totalSavings.absoluteValue} to the build. Assert you have stable internet connection.")
         }
-
-        logger.lifecycle(avoidanceMap.toString())
-
     }
 }
