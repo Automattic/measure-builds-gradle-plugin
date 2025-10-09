2e531ed3d8168d5c792e02d89fb7a039f24f386f
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-10-09 15:07:25 +0200

    Fix static code analysis issues

--- measure-builds/src/test/java/com/automattic/android/measure/MachineDataProviderTest.kt
+++ measure-builds/src/test/java/com/automattic/android/measure/MachineDataProviderTest.kt
@@ -2,7 +2,6 @@ package com.automattic.android.measure
 
 import com.automattic.android.measure.providers.MachineDataProvider
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 class MachineDataProviderTest {
 
@@ -16,9 +15,12 @@ class MachineDataProviderTest {
 
         // Verify it's a known architecture format
         val knownArchitectures = setOf(
-            "aarch64", "arm64",  // ARM 64-bit
-            "x86_64", "amd64",   // x86 64-bit
-            "x86", "i386"         // x86 32-bit
+            "aarch64",
+            "arm64", // ARM 64-bit
+            "x86_64",
+            "amd64", // x86 64-bit
+            "x86",
+            "i386" // x86 32-bit
         )
         assert(architecture in knownArchitectures) {
             "Unknown architecture: $architecture on OS: $os"
